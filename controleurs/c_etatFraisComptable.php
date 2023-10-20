43d13d2c088c100fef1e5c417a0249857a2e88b5
MaximeMaheswaran <pro.ma.maxime@gmail.com>
2023-10-20 11:48:09 +0200

    mois est mis a sa place

--- controleurs/c_etatFraisComptable.php
+++ controleurs/c_etatFraisComptable.php
@@ -12,7 +12,7 @@ if (isset($_SESSION)) {
 					// les mois étant triés décroissants
 					$lesCles = array_keys($lesMois);
 					$moisASelectionner = $lesCles[0];
-					include("vues/v_listeMois.php");
+					include("vues/v_tabFicheFraisVisiteur.php");
 					break;
 				}
 			case 'voirEtatFrais': {
@@ -31,6 +31,7 @@ if (isset($_SESSION)) {
 					$dateModif =  $lesInfosFicheFrais['dateModif'];
 					$dateModif =  dateAnglaisVersFrancais($dateModif);
 					include("vues/v_etat.php");
+					break;
 				}
 
 			case 'voirEtatFraisComptable': {
@@ -39,7 +40,9 @@ if (isset($_SESSION)) {
 					$idVisiteur = $_POST['idVisiteur'];
 					$lesFraisForfait = $pdo->getLesFraisForfait($idVisiteur, $mois);
 					$lesFraisHorsForfait = $pdo->getLesFraisHorsForfait($idVisiteur, $mois);
-					include("vues/v_listeFraisForfait.php");
+					include("vues/v_tabFicheFraisVisiteur.php");
+					include("vues/v_listeFraisForfaitComptable.php");
+					break;
 				}
 			case 'tabFicheFraisVisiteur': {
 					$lesMois = $pdo->getLesMoisDisponibles($idVisiteur);
--- controleurs/c_gererFraisComptable.php
+++ controleurs/c_gererFraisComptable.php
@@ -3,22 +3,21 @@
 if (isset($_SESSION)) {
 	if ($pdo->verifPersonneId($_SESSION['idVisiteur']) == 2) {
 		include("vues/v_sommaireComptable.php");
-$idVisiteur = $_SESSION['idVisiteur'];
-$mois = getMois(date("d/m/Y"));
-$numAnnee = substr($mois, 0, 4);
-$numMois = substr($mois, 4, 2);
-$action = $_REQUEST['action'];
-switch ($action) {
-	case 'validerFraisComptable': {
-			$ligne = $pdo->getFicheFraisCL();
-			include("vues/v_validerFrais.php");
-			break;
+		$idVisiteur = $_SESSION['idVisiteur'];
+		$mois = getMois(date("d/m/Y"));
+		$numAnnee = substr($mois, 0, 4);
+		$numMois = substr($mois, 4, 2);
+		$action = $_REQUEST['action'];
+		switch ($action) {
+			case 'validerFraisComptable': {
+					$ligne = $pdo->getFicheFraisCL();
+					include("vues/v_validerFrais.php");
+					break;
+				}
 		}
-}
 	} else {
 		header("Location: index.php");
 	}
 } else {
 	header("Location: index.php");
 }
-
